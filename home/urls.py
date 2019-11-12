fb56394ccbfa88952bc1b77aae0ad891737a8fc1
Mohith7548 <mohith7548@gmail.com>
2019-11-12 20:14:13 +0530

    Home page done

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import home_page
+from .views import home_page, register_page, login_page, logout_page
 
 urlpatterns = [
-    path('', home_page),
+    path('', home_page, name='home'),
+    path('register/', register_page, name='register'),
+    path('login/', login_page, name='login'),
+    path('logout/', logout_page, name='logout'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render, HttpResponse
 
 def home_page(request):
     return render(request, 'home.html')
+
+
+def register_page(request):
+    return HttpResponse('<h1> Register </h1>')
+
+
+def login_page(request):
+    return HttpResponse('<h1> Login </h1>')
+
+
+def logout_page(request):
+    return HttpResponse('<h1> Logout </h1>')
