819b541a87ba99ecd10ec585ed18cc3cb5dca60d
Mohith7548 <mohith7548@gmail.com>
2019-11-14 21:02:18 +0530

    hosts added

--- graphical_pwd_auth/settings.py
+++ graphical_pwd_auth/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = '1_vkli_wb6j3l_v5gl3rd889j4!6%b&-bn$qhkobzb5h#bih8f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
