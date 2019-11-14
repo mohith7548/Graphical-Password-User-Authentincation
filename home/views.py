d49537031584396280280b47cdbee2fdb1887f27
Mohith7548 <mohith7548@gmail.com>
2019-11-14 14:36:30 +0530

    get_pwd_images function added

--- home/views.py
+++ home/views.py
@@ -6,6 +6,17 @@ from graphical_pwd_auth.settings import N
 import random
 
 
+def get_pwd_imgs():
+    # These images are just to confuse the attacker
+    images = random.sample(range(1, 50), N * N)
+    print(images)
+    p_images = []
+    for i in range(0, N * N, 5):
+        p_images.append(images[i:i+5])
+    print(p_images)
+    return p_images
+
+
 def home_page(request):
     return render(request, 'home.html')
 
@@ -19,20 +30,12 @@ def register_page(request):
             user = User.objects.create_user(username=username, password=password)
             messages.success(request, 'Account created successfully!')
         except Exception:
-            # print(e)
             messages.warning(request, 'Error while creating Account!')
         
         return redirect('home')
     else:
-        images = random.sample(range(1, 50), N * N)
-        print(images)
-        p_images = []
-        for i in range(0, N * N, 5):
-            p_images.append(images[i:i+5])
-        print(p_images)
-
         data = {
-            'p_images': p_images,
+            'p_images': get_pwd_imgs(),
         }
         return render(request, 'register.html', context=data)
 
@@ -51,15 +54,8 @@ def login_page(request):
             messages.warning(request, 'Wrong credentials!')
             return redirect('login')
     else:
-        images = random.sample(range(1, 50), N * N)
-        print(images)
-        p_images = []
-        for i in range(0, N * N, 5):
-            p_images.append(images[i:i+5])
-        print(p_images)
-
         data = {
-            'p_images': p_images,
+            'p_images': get_pwd_imgs(),
         }
         return render(request, 'login.html', context=data)
 
